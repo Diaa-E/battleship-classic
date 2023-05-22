a08821cf9eac224a7c230ff0c0746a5756ccfeaf
Diaa-E <blarb21@yahoo.com>
2023-05-22 15:25:29 +0300

    Add ship module skeleton

---
+++ src/ship.js
@@ -0,0 +1,44 @@
+"use strict";
+
+export function ship(name, length, pivot)
+{
+    const hits = [];
+    const position = [];
+    let isSunk = false;
+    let isVertical = true;
+
+    function rotate()
+    {
+    
+    }
+
+    function checkHits(hits)
+    {
+
+    }
+
+    function registerHit(hit)
+    {
+
+    }
+
+    function move(newPivot)
+    {
+
+    }
+
+    function evaluatePosition(newPivot, vert)
+    {
+
+    }
+
+    return {
+        name: name,
+        hits: hits,
+        position: position,
+        isSunk: isSunk,
+        rotate: rotate,
+        checkHits: checkHits,
+        move: move,
+    }
+}
