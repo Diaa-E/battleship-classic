739cc1382d1a9b0b4023a23df163a119fba9ac99
Diaa-E <blarb21@yahoo.com>
2023-05-22 15:38:25 +0300

    Add tests for ship module

---
+++ src/ship.test.js
@@ -0,0 +1,49 @@
+import { ship } from "./ship";
+
+
+test("Evaluates position", () => {
+    
+    const carrier = ship("Carrier", 5, [0, 3]);
+    
+    expect(carrier.position).toEqual([[0, 3], [0, 4], [0, 5], [0, 6], [0, 7]]);
+});
+
+test("Registers a single hit", () => {
+
+    const carrier = ship("Carrier", 5, [0, 3]);
+    carrier.checkHits([[0, 5]]);
+
+    expect(carrier.hits).toEqual([[0, 5]]);
+});
+
+test("Registers multiple hits", () => {
+
+    const carrier = ship("Carrier", 5, [0, 3]);
+    carrier.checkHits([[0, 5]]);
+
+    expect(carrier.hits).toEqual([[0, 5], [0, 4], [0, 6]]);
+});
+
+test("Ignores missed shots", () => {
+
+    const carrier = ship("Carrier", 5, [0, 3]);
+    carrier.checkHits([[0, 0]]);
+
+    expect(carrier.hits).toEqual([]);
+});
+
+test("Moves to a new location", () => {
+
+    const carrier = ship("Carrier", 5, [0, 3]);
+    carrier.move([2, 3]);
+
+    expect(carrier.position).toEqual([[2, 3], [2, 4], [2, 5], [2, 6], [2, 7]]);
+});
+
+test("Sinks when all squares have been hit", () => {
+
+    const carrier = ship("Carrier", 5, [0, 3]);
+    carrier.checkHits([[0, 3], [0, 4], [0, 5], [0, 6], [0, 7]]);
+
+    expect(carrier.isSunk).toBeTruthy();
+});
