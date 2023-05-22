330d6a00316608e9a37a18ceb2937a5806324aa7
Diaa-E <blarb21@yahoo.com>
2023-05-22 18:25:31 +0300

    Test updating ship location

--- src/ship.test.js
+++ src/ship.test.js
@@ -1,11 +1,18 @@
 import {expect, jest, test} from '@jest/globals';
 import { ship } from "./ship";
 
-test("Evaluates position", () => {
+test("Initializes position", () => {
     
     const carrier = ship("Carrier", 5, [0, 3]);
     
     expect(carrier.position).toStrictEqual([[0, 3], [0, 4], [0, 5], [0, 6], [0, 7]]);
+});
+
+test("Moves to a new location", () => {
+
+    const carrier = ship("Carrier", 5, [0, 3]);
+
+    expect(carrier.move([2, 3])).toStrictEqual([[2, 3], [2, 4], [2, 5], [2, 6], [2, 7]]);
 });
 
 test("Registers a single hit", () => {
@@ -27,13 +34,6 @@ test("Ignores missed shots", () => {
     const carrier = ship("Carrier", 5, [0, 3]);
 
     expect(carrier.checkHits([[0, 0]])).toEqual([]);
-});
-
-test("Moves to a new location", () => {
-
-    const carrier = ship("Carrier", 5, [0, 3]);
-
-    expect(carrier.move([2, 3])).toEqual([[2, 3], [2, 4], [2, 5], [2, 6], [2, 7]]);
 });
 
 test("Sinks when all squares have been hit", () => {
