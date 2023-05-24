1d27661b93d24a5567fbab4272a2654a025efb64
Diaa-E <blarb21@yahoo.com>
2023-05-24 10:05:18 +0300

    Test hits property instead of function return

--- src/ship.test.js
+++ src/ship.test.js
@@ -35,22 +35,25 @@ test("Rotates +90d", () => {
 test("Registers a single hit", () => {
 
     const carrier = ship("Carrier", 5, [0, 3]);
+    carrier.checkHits([[0, 5]]);
 
-    expect(carrier.checkHits([[0, 5]])).toEqual([[0, 5]]);
+    expect(carrier.hits).toStrictEqual([[0, 5]]);
 });
 
 test("Registers multiple hits", () => {
 
     const carrier = ship("Carrier", 5, [0, 3]);
-
-    expect(carrier.checkHits([[0, 5]])).toEqual([[0, 5], [0, 4], [0, 6]]);
+    carrier.checkHits([[0, 5], [0, 4], [0, 6]]);
+    
+    expect(carrier.hits).toStrictEqual([[0, 5], [0, 4], [0, 6]]);
 });
 
 test("Ignores missed shots", () => {
 
     const carrier = ship("Carrier", 5, [0, 3]);
+    carrier.checkHits([[0, 0], [0, 5]]);
 
-    expect(carrier.checkHits([[0, 0]])).toEqual([]);
+    expect(carrier.hits).toStrictEqual([[0, 5]]);
 });
 
 test("Sinks when all squares have been hit", () => {
