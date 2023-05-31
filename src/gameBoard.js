bc232a7a3252c0905a83e5baf7eea2db93f07a42
Diaa-E <blarb21@yahoo.com>
2023-05-31 21:22:12 +0300

    Create ships and initialize board

---
+++ src/gameBoard.js
@@ -0,0 +1,69 @@
+"use strict";
+
+import { ship } from "./ship";
+
+export function gameBoard(shipList, boardSize)
+{
+    const _ships = [];
+    let board;
+    const _TOKENS = {
+        empty: "[]",
+        damaged: "X",
+        missed: "O",
+    }
+
+    _initBoard(boardSize, shipList);
+
+    function _createShips(shipList)
+    {
+        shipList.forEach((element, index) => {
+
+            _ships.push(ship(element.name, element.shipLength, [index, 0], true));
+        });
+    }
+
+    function updateBoard()
+    {
+        updateEmpty(); //in case a ship is moved
+        updateShip();
+        updateDamage();
+    }
+
+    function _initBoard(size, shipList)
+    {
+        board = Array(size).fill(Array(size).fill(_TOKENS.empty));
+        _createShips(shipList);
+        updateBoard();
+    }
+
+    function updateShip()
+    {
+        _ships.forEach(element => {
+
+            element.position.forEach(pair => {
+
+                board[pair[1]][pair[0]] = element;
+            });
+        });
+    }
+
+    function updateEmpty()
+    {
+        board.forEach(row => {
+
+            row.forEach(square => {
+
+                square = _TOKENS.empty;
+            });
+        });
+    }
+
+    function updateDamage()
+    {
+
+    }
+
+    return {
+        get board(){ return board },
+    }
+}
