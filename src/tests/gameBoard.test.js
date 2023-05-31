a6b6a1812448a3a605438effbaca948da14b1a3f
Diaa-E <blarb21@yahoo.com>
2023-06-01 00:05:17 +0300

    Add tests for moving ships

--- src/tests/gameBoard.test.js
+++ src/tests/gameBoard.test.js
@@ -1,6 +1,12 @@
 import {expect, jest, test} from '@jest/globals';
 import {gameBoard} from "../gameBoard";
 
+const tokens = {
+    empty: "[]",
+    damaged: "X",
+    missed: "O",
+}
+
 const SHIPS_LIST = [
     {
         shipLength: 5,
@@ -24,9 +30,9 @@ const SHIPS_LIST = [
     }
 ];
 
-test("Creates ship objects from the given list placed verticall next to each other.", () => {
+test("Creates ship objects from the given list placed vertically next to each other.", () => {
 
-    const board1 = gameBoard(SHIPS_LIST, 10);
+    const board1 = gameBoard(SHIPS_LIST, 10, tokens);
 
     SHIPS_LIST.forEach((element, index) => {
 
@@ -35,3 +41,37 @@ test("Creates ship objects from the given list placed verticall next to each oth
     });
 });
 
+test("Moves ship to a new position", () => {
+
+    const board1 = gameBoard([SHIPS_LIST[0]], 10, tokens);
+    board1.moveShip(board1[0][0], [1, 2], false);
+
+    expect(board1.board[0][0]).toStrictEqual(tokens.empty);
+    expect(board1[2][1].NAME).toStrictEqual(SHIPS_LIST[0].name);
+});
+
+test("Moves ship to a new position by selecting any part of the ship", () => {
+
+    const board1 = gameBoard([SHIPS_LIST[0]], 10, tokens);
+    board1.moveShip(board1[0][3], [1, 2], false);
+
+    expect(board1.board[0][0]).toStrictEqual(tokens.empty);
+    expect(board1[2][1].NAME).toStrictEqual(SHIPS_LIST[0].name);
+});
+
+test("Does not move ship over another ship", () => {
+
+    const board1 = gameBoard(SHIPS_LIST, 10, tokens);
+    board1.moveShip(board1[0][0], [1, 0], false);
+
+    expect(board1[0][0].NAME).toStrictEqual(SHIPS_LIST[0].name);
+    expect(board1[1][0].NAME).toStrictEqual(SHIPS_LIST[1].name);
+});
+
+test("Does not mvoe ship out of the board", () => {
+
+    const board1 = gameBoard([SHIPS_LIST[0]], 10, tokens);
+    board1.moveShip(board1[0][0], [0, 7], false);
+    expect(board1[0][0].NAME).toStrictEqual(SHIPS_LIST[0].name);
+    expect(board1[7][0]).toStrictEqual(tokens.empty);
+});
