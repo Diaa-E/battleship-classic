9546b6d86ebc9a951196b82967c7af5ae35d2d63
Diaa-E <blarb21@yahoo.com>
2023-05-31 21:23:07 +0300

    Change object property to avoid confusion, Fix assertion typo

--- src/tests/gameBoard.test.js
+++ src/tests/gameBoard.test.js
@@ -3,35 +3,35 @@ import {gameBoard} from "../gameBoard";
 
 const SHIPS_LIST = [
     {
-        length: 5,
+        shipLength: 5,
         name: "Carrier"
     },
     {
-        length: 4,
+        shipLength: 4,
         name: "Battleship"
     },
     {
-        length: 3,
+        shipLength: 3,
         name: "Submarine"
     },
     {
-        length: 3,
+        shipLength: 3,
         name: "Cruiser"
     },
     {
-        length: 2,
+        shipLength: 2,
         name: "Destroyer"
     }
 ];
 
 test("Creates ship objects from the given list placed verticall next to each other.", () => {
 
-    const board1 = gameBoard(SHIPS_LIST);
+    const board1 = gameBoard(SHIPS_LIST, 10);
 
     SHIPS_LIST.forEach((element, index) => {
 
-        expect(board1.board[0][index].name).toBeInstanceOf(element.name);
-        expect(board1.board[0][index].length).toBeInstanceOf(element.length);
+        expect(board1.board[0][index].NAME).toStrictEqual(element.name);
+        expect(board1.board[0][index].LENGTH).toStrictEqual(element.shipLength);
     });
 });
 
