8912b1a757212d98a69d5169b694bdc49b1cc4bf
Diaa-E <blarb21@yahoo.com>
2023-06-10 02:28:51 +0300

    Update tests to fit new API

--- src/tests/gameBoard.test.js
+++ src/tests/gameBoard.test.js
@@ -29,18 +29,18 @@ test("Board places ships vertically next to each other starting at (0, 0) after 
 
     const board1 = GameBoard(DEFAULT_SHIP_LIST, 10);
 
-    expect(board1.board[0][0].NAME).toStrictEqual(DEFAULT_SHIP_LIST[0].shipName);
-    expect(board1.board[0][1].NAME).toStrictEqual(DEFAULT_SHIP_LIST[1].shipName);
-    expect(board1.board[0][2].NAME).toStrictEqual(DEFAULT_SHIP_LIST[2].shipName);
-    expect(board1.board[0][3].NAME).toStrictEqual(DEFAULT_SHIP_LIST[3].shipName);
-    expect(board1.board[0][4].NAME).toStrictEqual(DEFAULT_SHIP_LIST[4].shipName);
+    expect(board1.getSquareAt([0, 0]).NAME).toStrictEqual(DEFAULT_SHIP_LIST[0].shipName);
+    expect(board1.getSquareAt([1, 0]).NAME).toStrictEqual(DEFAULT_SHIP_LIST[1].shipName);
+    expect(board1.getSquareAt([2, 0]).NAME).toStrictEqual(DEFAULT_SHIP_LIST[2].shipName);
+    expect(board1.getSquareAt([3, 0]).NAME).toStrictEqual(DEFAULT_SHIP_LIST[3].shipName);
+    expect(board1.getSquareAt([4, 0]).NAME).toStrictEqual(DEFAULT_SHIP_LIST[4].shipName);
 
-    expect(board1.board[0][5]).toStrictEqual(board1.PINS.empty);
-    expect(board1.board[5][0]).toStrictEqual(board1.PINS.empty);
-    expect(board1.board[4][1]).toStrictEqual(board1.PINS.empty);
-    expect(board1.board[3][2]).toStrictEqual(board1.PINS.empty);
-    expect(board1.board[3][3]).toStrictEqual(board1.PINS.empty);
-    expect(board1.board[2][4]).toStrictEqual(board1.PINS.empty);
+    expect(board1.getSquareAt([5, 0])).toStrictEqual(board1.PINS.empty);
+    expect(board1.getSquareAt([0, 5])).toStrictEqual(board1.PINS.empty);
+    expect(board1.getSquareAt([1, 4])).toStrictEqual(board1.PINS.empty);
+    expect(board1.getSquareAt([2, 3])).toStrictEqual(board1.PINS.empty);
+    expect(board1.getSquareAt([3, 3])).toStrictEqual(board1.PINS.empty);
+    expect(board1.getSquareAt([4, 2])).toStrictEqual(board1.PINS.empty);
 });
 
 // 2
@@ -49,9 +49,9 @@ test("Board handles missed shots correctly.", () => {
     const board1 = GameBoard(DEFAULT_SHIP_LIST, 10);
     board1.receiveAttack([[6, 6], [6, 7], [9, 9]]);
 
-    expect(board1.board[6][6]).toStrictEqual(board1.PINS.missed);
-    expect(board1.board[7][6]).toStrictEqual(board1.PINS.missed);
-    expect(board1.board[9][9]).toStrictEqual(board1.PINS.missed);
+    expect(board1.getSquareAt([6, 6])).toStrictEqual(board1.PINS.missed);
+    expect(board1.getSquareAt([6, 7])).toStrictEqual(board1.PINS.missed);
+    expect(board1.getSquareAt([9, 9])).toStrictEqual(board1.PINS.missed);
 });
 
 // 3
@@ -60,9 +60,9 @@ test("Board handles hit shots correctly.", () => {
     const board1 = GameBoard(DEFAULT_SHIP_LIST, 10);
     board1.receiveAttack([[0, 0], [1, 3], [4, 0]]);
 
-    expect(board1.board[0][0]).toStrictEqual(board1.PINS.damaged);
-    expect(board1.board[3][1]).toStrictEqual(board1.PINS.damaged);
-    expect(board1.board[0][4]).toStrictEqual(board1.PINS.damaged);
+    expect(board1.getSquareAt([0, 0])).toStrictEqual(board1.PINS.damaged);
+    expect(board1.getSquareAt([1, 3])).toStrictEqual(board1.PINS.damaged);
+    expect(board1.getSquareAt([4, 0])).toStrictEqual(board1.PINS.damaged);
 });
 
 // 4
@@ -71,46 +71,46 @@ test("Board handles mixed (includes hits and misses) attack correctly.", () => {
     const board1 = GameBoard(DEFAULT_SHIP_LIST, 10);
     board1.receiveAttack([[0, 0], [6, 7], [4, 0]]);
 
-    expect(board1.board[0][0]).toStrictEqual(board1.PINS.damaged);
-    expect(board1.board[7][6]).toStrictEqual(board1.PINS.missed);
-    expect(board1.board[0][4]).toStrictEqual(board1.PINS.damaged);
+    expect(board1.getSquareAt([0, 0])).toStrictEqual(board1.PINS.damaged);
+    expect(board1.getSquareAt([6, 7])).toStrictEqual(board1.PINS.missed);
+    expect(board1.getSquareAt([4, 0])).toStrictEqual(board1.PINS.damaged);
 });
 
 // 5
 test("Board moves a ship to a new position.", () => {
 
     const board1 = GameBoard(DEFAULT_SHIP_LIST, 10);
-    board1.moveShip(board1.board[0][0], [8, 0]);
+    board1.moveShip(board1.getSquareAt([0, 0]), [8, 0]);
 
-    expect(board1.board[0][0]).toStrictEqual(board1.PINS.empty);
-    expect(board1.board[0][8]).toBeInstanceOf(Object);
+    expect(board1.getSquareAt([0 ,0])).toStrictEqual(board1.PINS.empty);
+    expect(board1.getSquareAt([8, 0])).toBeInstanceOf(Object);
 });
 
 // 6
 test("Board rotates a ship.", () => {
 
     const board1 = GameBoard(DEFAULT_SHIP_LIST, 10);
-    board1.rotateShip(board1.board[0][4]);
+    board1.rotateShip(board1.getSquareAt([4, 0]));
 
-    expect(board1.board[0][4]).toBeInstanceOf(Object);
-    expect(board1.board[0][5]).toBeInstanceOf(Object);
-    expect(board1.board[1][4]).toStrictEqual(board1.PINS.empty);
+    expect(board1.getSquareAt([4, 0])).toBeInstanceOf(Object);
+    expect(board1.getSquareAt([5, 0])).toBeInstanceOf(Object);
+    expect(board1.getSquareAt([4, 1])).toStrictEqual(board1.PINS.empty);
 });
 
 // 7
 test("Board does not move ship in case of position conflict.", () => {
 
     const board1 = GameBoard(DEFAULT_SHIP_LIST, 10);
-    board1.moveShip(board1.board[0][0], [2, 0]);
+    board1.moveShip(board1.getSquareAt([0 ,0]), [2, 0]);
 
     for (let i = 0; i < DEFAULT_SHIP_LIST[0].shipLength; i ++)
     {
-        expect(board1.board[i][0].NAME).toStrictEqual(DEFAULT_SHIP_LIST[0].shipName);
+        expect(board1.getSquareAt([0, i]).NAME).toStrictEqual(DEFAULT_SHIP_LIST[0].shipName);
     }
 
     for (let i = 0; i < DEFAULT_SHIP_LIST.length; i ++)
     {
-        expect(board1.board[0][i].NAME).toStrictEqual(DEFAULT_SHIP_LIST[i].shipName);
+        expect(board1.getSquareAt([i, 0]).NAME).toStrictEqual(DEFAULT_SHIP_LIST[i].shipName);
     }
     
 });
@@ -119,16 +119,16 @@ test("Board does not move ship in case of position conflict.", () => {
 test("Board does not rotate ship in case of rotation conflict.", () => {
 
     const board1 = GameBoard(DEFAULT_SHIP_LIST, 10);
-    board1.rotateShip(board1.board[0][0]);
+    board1.rotateShip(board1.getSquareAt([0, 0]));
 
     for (let i = 0; i < DEFAULT_SHIP_LIST[0].shipLength; i ++)
     {
-        expect(board1.board[i][0].NAME).toStrictEqual(DEFAULT_SHIP_LIST[0].shipName);
+        expect(board1.getSquareAt([0, i]).NAME).toStrictEqual(DEFAULT_SHIP_LIST[0].shipName);
     }
 
     for (let i = 0; i < DEFAULT_SHIP_LIST.length; i ++)
     {
-        expect(board1.board[0][i].NAME).toStrictEqual(DEFAULT_SHIP_LIST[i].shipName);
+        expect(board1.getSquareAt([i, 0]).NAME).toStrictEqual(DEFAULT_SHIP_LIST[i].shipName);
     }
     
 });
@@ -137,16 +137,16 @@ test("Board does not rotate ship in case of rotation conflict.", () => {
 test("Board moves a ship to a location along it's old position", () => {
 
     const board1 = GameBoard(DEFAULT_SHIP_LIST, 10);
-    board1.moveShip(board1.board[0][0], [0, 2]);
+    board1.moveShip(board1.getSquareAt([0, 0]), [0, 2]);
 
     for (let i = 0; i < 2; i ++)
     {
-        expect(board1.board[i][0]).toStrictEqual(board1.PINS.empty);
+        expect(board1.getSquareAt([0, i])).toStrictEqual(board1.PINS.empty);
     }
 
     for (let i = 2; i < DEFAULT_SHIP_LIST[0].shipLength + 2; i ++)
     {
-        expect(board1.board[i][0].NAME).toStrictEqual(DEFAULT_SHIP_LIST[0].shipName);
+        expect(board1.getSquareAt([0, i]).NAME).toStrictEqual(DEFAULT_SHIP_LIST[0].shipName);
     }
 });
 
@@ -160,5 +160,5 @@ test("Game is over when a board's entire fleet is sunk.", () => {
     board1.receiveAttack([[3, 0], [3, 1], [3, 2]]);
     board1.receiveAttack([[4, 0], [4, 1], [9, 9]]);
 
-    expect(board1.board[9][9]).toStrictEqual(board1.PINS.empty);
+    expect(board1.getSquareAt([9 ,9])).toStrictEqual(board1.PINS.empty);
 });
