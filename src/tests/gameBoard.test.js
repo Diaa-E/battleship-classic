a5a4217d8c2e0b8c92cc995b2cd3ffc3214a74fd
Diaa-E <blarb21@yahoo.com>
2023-06-09 06:07:02 +0300

    Create tests for attack cases

--- src/tests/gameBoard.test.js
+++ src/tests/gameBoard.test.js
@@ -34,11 +34,44 @@ test("Board places ships vertically next to each other starting at (0, 0) after 
     expect(board1.board[0][2].NAME).toStrictEqual(DEFAULT_SHIP_LIST[2].shipName);
     expect(board1.board[0][3].NAME).toStrictEqual(DEFAULT_SHIP_LIST[3].shipName);
     expect(board1.board[0][4].NAME).toStrictEqual(DEFAULT_SHIP_LIST[4].shipName);
-    
+
     expect(board1.board[0][5]).toStrictEqual(board1.TOKENS.empty);
     expect(board1.board[5][0]).toStrictEqual(board1.TOKENS.empty);
     expect(board1.board[4][1]).toStrictEqual(board1.TOKENS.empty);
     expect(board1.board[3][2]).toStrictEqual(board1.TOKENS.empty);
     expect(board1.board[3][3]).toStrictEqual(board1.TOKENS.empty);
     expect(board1.board[2][4]).toStrictEqual(board1.TOKENS.empty);
+});
+
+// 2
+test("Board handles missed shots correctly.", () => {
+
+    const board1 = GameBoard(DEFAULT_SHIP_LIST, 10);
+    board1.receiveAttack([[6, 6], [6, 7], [9, 9]]);
+
+    expect(board1.board[6][6]).toStrictEqual(board1.TOKENS.missed);
+    expect(board1.board[6][7]).toStrictEqual(board1.TOKENS.missed);
+    expect(board1.board[9][9]).toStrictEqual(board1.TOKENS.missed);
+});
+
+// 3
+test("Board handles hit shots correctly.", () => {
+
+    const board1 = GameBoard(DEFAULT_SHIP_LIST, 10);
+    board1.receiveAttack([[0, 0], [1, 3], [4, 0]]);
+
+    expect(board1.board[0][0]).toStrictEqual(board1.TOKENS.damaged);
+    expect(board1.board[3][1]).toStrictEqual(board1.TOKENS.damaged);
+    expect(board1.board[0][4]).toStrictEqual(board1.TOKENS.damaged);
+});
+
+// 4
+test("Board handles mixed (includes hits and misses) attack correctly.", () => {
+
+    const board1 = GameBoard(DEFAULT_SHIP_LIST, 10);
+    board1.receiveAttack([[0, 0], [6, 7], [4, 0]]);
+
+    expect(board1.board[0][0]).toStrictEqual(board1.TOKENS.damaged);
+    expect(board1.board[7][6]).toStrictEqual(board1.TOKENS.missed);
+    expect(board1.board[0][4]).toStrictEqual(board1.TOKENS.damaged);
 });
