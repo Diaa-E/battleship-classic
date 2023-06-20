043cdbffb345df5653420354efd184a8c18cc6f9
Diaa-E <blarb21@yahoo.com>
2023-06-20 09:05:36 +0300

    Create test for pin box object

--- src/tests/gameBoard.test.js
+++ src/tests/gameBoard.test.js
@@ -1,7 +1,12 @@
 import {expect, jest, test} from '@jest/globals';
-import {GameBoard, emptyBoard} from "../gameBoard";
+import {GameBoard, EmptyBoard, pinBox} from "../gameBoard";
 
 test("EmptyBoard returns a square 2D array filled with an empty pin character", () => {
 
-    expect(emptyBoard(3, "E")).toStrictEqual([["E", "E", "E"], ["E", "E", "E"], ["E", "E", "E"]]);
+    expect(EmptyBoard(3, "E")).toStrictEqual([["E", "E", "E"], ["E", "E", "E"], ["E", "E", "E"]]);
+});
+
+test("pinBox returns an object with pins for empty, missed and hit squares", () => {
+
+    expect(pinBox("E", "M", "H")).toStrictEqual({empty: "E", missed: "M", hit: "H"});
 });
