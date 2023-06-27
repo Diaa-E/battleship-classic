7cb583928fb1515362dc14f684b98d5528c57554
Diaa-E <blarb21@yahoo.com>
2023-06-27 08:59:38 +0300

    Sort board squares into 3 categories

--- src/aiBrain.js
+++ src/aiBrain.js
@@ -1,10 +1,46 @@
 "use strict";
 
-export {AiBrain}
+import { encodeCoord } from "./positionUtility";
+
+export {AiBrain, sortBoard};
 
 function AiBrain(rules)
 {
+    let boardStatus = {};
+
     return {
-        
+
+    }
+}
+
+function sortBoard(board, emptyPin, damagedPin)
+{
+    const availableSquares = [];
+    const damagedSquares = [];
+    const otherSquares = []; //sunken ships and missed shots, they don't affect decision making
+
+    for (let x = 0; x < board.length; x++)
+    {
+        for (let y = 0; y < board.length; y ++)
+        {
+            if (board[y][x] === emptyPin)
+            {
+                availableSquares.push(encodeCoord([x, y]));
+            }
+            else if (board[y][x] === damagedPin)
+            {
+                damagedSquares.push(encodeCoord([x, y]));
+            }
+            else
+            {
+                otherSquares.push(encodeCoord([x, y]));
+            }
+        }
+    }
+
+    return {
+        availableSquares: availableSquares,
+        damagedSquares: damagedSquares,
+        otherSquares: otherSquares
     }
 }
