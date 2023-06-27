4b3467670ca7a7cd8329d2ad3a0bfada520f76cb
Diaa-E <blarb21@yahoo.com>
2023-06-27 05:19:41 +0300

    Create main game object

---
+++ src/game.js
@@ -0,0 +1,12 @@
+"use strict";
+
+export function Game(playerName, gameMode)
+{
+    let aiTurn = pickRandomTurn === 0 ? false : true;
+
+}
+
+function pickRandomTurn()
+{
+    return Math.round(Math.random());
+}
