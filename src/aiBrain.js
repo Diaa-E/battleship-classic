a96027aedcfa8497ca2c72992282432ed6b2eaf4
Diaa-E <blarb21@yahoo.com>
2023-06-28 04:28:39 +0300

    Remove general board sort function

--- src/aiBrain.js
+++ src/aiBrain.js
@@ -2,40 +2,12 @@
 
 import { encodeCoord } from "./positionUtility";
 
-export {AiBrain, sortBoard, checkerEmptySquares};
+export {AiBrain, checkerEmptySquares};
 
 function AiBrain(rules)
 {
-    let boardStatus = {};
-
     return {
 
-    }
-}
-
-function sortBoard(board, emptyPin, damagedPin)
-{
-    const availableSquares = [];
-    const damagedSquares = [];
-
-    for (let x = 0; x < board.length; x++)
-    {
-        for (let y = 0; y < board.length; y ++)
-        {
-            if (board[y][x] === emptyPin)
-            {
-                availableSquares.push(encodeCoord([x, y]));
-            }
-            else if (board[y][x] === damagedPin)
-            {
-                damagedSquares.push(encodeCoord([x, y]));
-            }
-        }
-    }
-
-    return {
-        availableSquares: availableSquares,
-        damagedSquares: damagedSquares,
     }
 }
 
--- src/tests/aiBrain.test.js
+++ src/tests/aiBrain.test.js
@@ -1,47 +1,5 @@
 import {expect, jest, test} from '@jest/globals';
-import { sortBoard, checkerEmptySquares } from '../aiBrain';
-
-test("AI Brain sorts empty squares correctly", () => {
-
-    const board = [
-        ["E", "E", "E"],
-        ["E", "E", "E"],
-        ["E", "E", "E"],
-    ];
-
-    expect(sortBoard(board, "E", "D")).toStrictEqual({
-        availableSquares: ["0,0", "0,1", "0,2", "1,0", "1,1", "1,2", "2,0", "2,1", "2,2"],
-        damagedSquares: [],
-    });
-});
-
-test("AI Brain sorts damaged squares correctly", () => {
-
-    const board = [
-        ["E", "D", "E"],
-        ["D", "E", "D"],
-        ["E", "E", "E"],
-    ];
-
-    expect(sortBoard(board, "E", "D")).toStrictEqual({
-        availableSquares: ["0,0", "0,2", "1,1", "1,2", "2,0", "2,2"],
-        damagedSquares: ["0,1", "1,0", "2,1"],
-    })
-});
-
-test("AI Brain ignores ship and missed squares", () => {
-    
-    const board = [
-        ["E", "M", "E"],
-        ["D", { }, "D"],
-        ["M", { }, "E"],
-    ];
-
-    expect(sortBoard(board, "E", "D")).toStrictEqual({
-        availableSquares: ["0,0", "2,0", "2,2"],
-        damagedSquares: ["0,1", "2,1"],
-    });
-});
+import { checkerEmptySquares } from '../aiBrain';
 
 test("AI Brain halves the gameboard by checkering", () => {
 
