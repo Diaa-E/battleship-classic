eb109865c4942de40c9ef2ddfbc2c8656d563fa3
Diaa-E <blarb21@yahoo.com>
2023-06-28 04:48:37 +0300

    Add starting from first square to assertions

--- src/tests/aiBrain.test.js
+++ src/tests/aiBrain.test.js
@@ -10,6 +10,7 @@ test("AI Brain halves the gameboard by checkering", () => {
     ];
 
     expect(checkerEmptySquares(board, "E", true)).toStrictEqual(["0,1", "1,0", "1,2", "2,1"]);
+    expect(checkerEmptySquares(board, "E", false)).toStrictEqual(["0,0", "0,2", "1,1", "2,0", "2,2"]);
 });
 
 test("AI Brain halves the gameboard by checkering and ignores occupied squares", () => {
@@ -22,4 +23,5 @@ test("AI Brain halves the gameboard by checkering and ignores occupied squares",
     ];
 
     expect(checkerEmptySquares(board, "E", true)).toStrictEqual(["0,3", "2,3", "3,0", "3,2"]);
+    expect(checkerEmptySquares(board, "E", false)).toStrictEqual(["0,0", "0,2", "1,1", "1,3", "2,0", "2,2", "3,1", "3,3"]);
 });
