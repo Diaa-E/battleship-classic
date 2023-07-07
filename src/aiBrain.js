8981f7367f190366eb6c436a812f1e51927a10b2
Diaa-E <blarb21@yahoo.com>
2023-07-07 16:51:30 +0300

    Remove unused board analysis functions

--- src/aiBrain.js
+++ src/aiBrain.js
@@ -2,7 +2,7 @@
 
 import { encodeCoord } from "./positionUtility";
 
-export {AiBrain, checkerEmptySquares, scrapeDamagedSquares, createWeightedBoard, bustRows, bustColumns};
+export {AiBrain, createWeightedBoard, bustRows, bustColumns};
 
 function AiBrain(rules)
 {
@@ -17,38 +17,6 @@ function AiBrain(rules)
     return {
 
     }
-}
-
-function checkerEmptySquares(board, emptyPin, skipTopLeft)
-{
-    const checkeredBoard = [];
-    let skipFirst = skipTopLeft;
-
-    for (let x = 0; x < board.length; x++)
-    {
-        for (let y = skipFirst? 1 : 0; y < board.length; y += 2)
-        {
-            if (board[y][x] === emptyPin) checkeredBoard.push(encodeCoord([x, y]));
-        }
-        skipFirst = !skipFirst;
-    }
-
-    return checkeredBoard;
-}
-
-function scrapeDamagedSquares(board, damagedPin)
-{
-    const damaged = []
-
-    for (let x = 0; x < board.length; x++)
-    {
-        for (let y = 0; y < board.length; y++)
-        {
-            if (board[y][x] === damagedPin) damaged.push(encodeCoord([x, y]));
-        }
-    }
-
-    return damaged;
 }
 
 function bustRows(blockLength, weightedBoard, weights)
