1e9b2f6a86604a881d6e96406912b31c9fb22d7f
Diaa-E <blarb21@yahoo.com>
2023-07-18 11:38:12 +0300

    Add funcitons to update screen and receive input from user

--- src/screen.js
+++ src/screen.js
@@ -4,7 +4,33 @@ export { Screen };
 
 function Screen()
 {
+    function refresh(playerBoard, AiBoard)
+    {
+        printToConsole(playerBoard, AiBoard);
+    }
+
+    function getUserInput(promptMessage)
+    {
+        return getInput(promptMessage);
+    }
+
     return {
 
+        refresh,
+        getUserInput
     }
+}
+
+function printToConsole(playerBoard, AiBoard)
+{
+    console.log("Your Board");
+    console.table(playerBoard);
+
+    console.log("AI's board");
+    console.table(AiBoard);
+}
+
+function getInput(promptMEssage)
+{
+    return prompt(promptMEssage);
 }
