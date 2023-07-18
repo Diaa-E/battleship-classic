d2352c40a9630f703dd710ad52bddd98a33135b2
Diaa-E <blarb21@yahoo.com>
2023-07-18 14:30:37 +0300

    Add function to play AI's turn, pass pinbox to lower levels

--- src/game.js
+++ src/game.js
@@ -5,11 +5,13 @@ import { pickGameMode } from "./rules";
 
 export function Game(playerName, gameModeNumber)
 {
+    let pinBox = PinBox("E", "M", "H", "S");
     const rules = pickGameMode(gameModeNumber);
     let aiTurn = pickRandomTurn === 0 ? false : true;
+    let gameOver = false;
     const players = [
-        Player(playerName, false, rules),
-        Player("Player2", true, rules)
+        Player(playerName, false, rules, pinBox),
+        Player("Player2", true, rules, pinBox)
     ]
     
     function switchTurn()
@@ -17,14 +19,42 @@ export function Game(playerName, gameModeNumber)
         aiTurn = !aiTurn;
     }
 
+    function aiPlay()
+    {
+        const attacks = players[1].aiBrain.getNextAttack(players[0].board, pinBox, players[0].fleet, players[1].getAvailableShots());
+        
+        attacks.forEach(attack => {
+            players[0].receiveAttack(attack);
+        });
+        switchTurn();
+    }
+
+    function playerPlay()
+    {
+
+    }
+
     return {
         get aiTurn(){ return aiTurn },
+        get gameOver(){ return gameOver },
 
         switchTurn,
+        aiPlay,
+        playerPlay,
     };
 }
 
 function pickRandomTurn()
 {
     return Math.round(Math.random());
+}
+
+function PinBox(emptyPin, missedPin, hitPin, sunkPin)
+{
+    return {
+        empty: emptyPin,
+        missed: missedPin,
+        hit: hitPin,
+        sunk: sunkPin,
+    };
 }
