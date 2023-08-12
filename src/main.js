c9b45468786879ee89925ec57fcb03e966218fab
Diaa-E <blarb21@yahoo.com>
2023-08-13 02:11:59 +0300

    Test finished features

--- src/main.js
+++ src/main.js
@@ -1,12 +1,87 @@
 "use strict";
 
 import { Game } from "./game";
-import { Screen } from "./screen";
+import { ConsoleScreen, uiScreen } from "./screen";
+import "./css/style.css";
 
-newConsoleGame();
+newGame();
+
+function newGame()
+{
+    const game = Game("Human", 0);
+    const screen = uiScreen(game.rules.BOARD_SIZE);
+    screen.mount();
+
+    screen.setAiName(game.aiName);
+    screen.setPlayerName(game.humanName);
+    screen.initBoards(game.humanBoard, game.aiBoard);
+    screen.disableFireButton();
+    screen.openFleetEditor();
+    screen.refreshEditorBoard(game.humanBoard);
+    screen.highLightShip(game.humanFleet.ships[0].position);
+
+    document.addEventListener("currentShipChanged", (e) => {
+
+        game.humanFleet.ships.forEach(ship => {
+
+            screen.unhighLightShip(ship.position);
+        });
+        screen.highLightShip(game.humanFleet.ships[e.detail.shipIndex].position);
+    });
+
+    document.addEventListener("shipMoved", (e) => {
+
+        game.movePlayerShip(game.humanFleet.ships[e.detail.shipIndex].position[0].coord, e.detail.newPivot);
+        screen.refreshEditorBoard(game.humanBoard);
+        screen.highLightShip(game.humanFleet.ships[e.detail.shipIndex].position);
+        screen.initBoards(game.humanBoard, game.aiBoard);
+    });
+
+    document.addEventListener("shipRotated", (e) => {
+
+        game.rotatePlayerShip(game.humanFleet.ships[e.detail.shipIndex].position[0].coord);
+        screen.refreshEditorBoard(game.humanBoard);
+        screen.highLightShip(game.humanFleet.ships[e.detail.shipIndex].position);
+        screen.initBoards(game.humanBoard, game.aiBoard);
+    })
+    // game.humanFleet.ships.forEach(ship => {
+
+    //     ship.position.forEach(square => {
+
+    //         screen.refreshPlayerBoard(game.pinBox, game.humanBoard, decodeCoord(square.coord));
+    //     });
+    // });
+}
 
 function newConsoleGame()
 {
-    const screen = Screen();
-    const game = Game(screen.getUserInput("Enter your name"), +screen.getUserInput("Choose game mode:\n0 for normal \n1 for advanced"));
+    const game = Game("d", 0);
+    const screen = ConsoleScreen(game.pinBox);
+
+    screen.refresh(game.humanBoard, game.aiBoard, game.aiTurn);
+
+    while (!game.gameOver)
+    {
+        if (game.aiTurn)
+        {
+            game.aiPlay();
+        }
+        else
+        {
+            const attacks = [];
+
+            for (let i = 0; i < game.humanAvailableShots; i++)
+            {
+                attacks.push(screen.getUserInput(`Plot your #${i} shot in the x,y format:`));
+            }
+
+            game.humanPlay(attacks);
+        }
+
+        screen.refresh(game.humanBoard, game.aiBoard, game.aiTurn);
+    }
+
+    console.log(`${game.winner} wins!`);
+    console.log(`${game.aiName} fired ${game.getTotalShots().ai} shots.`);
+    console.log(`${game.humanName} fired ${game.getTotalShots().human} shots.`);
 }
